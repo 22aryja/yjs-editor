94f94b3767cf8d829520a769ed101b5c17e467f3
22aryja <dagerson19@mail.ru>
2025-10-19 16:28:45 +0500

    feat: added yjs

--- fe/src/Editor.tsx
+++ fe/src/Editor.tsx
@@ -3,6 +3,9 @@ import { BlockNoteView } from '@blocknote/mantine';
 import '@blocknote/mantine/style.css';
 import '@blocknote/core/fonts/inter.css';
 import './editor.css';
+import * as Y from 'yjs';
+
+const doc = new Y.Doc();
 
 export default function Editor() {
     const editor = useCreateBlockNote();
